4b32fefacbbdf5237f7278524fd93750f3adcb69
jincheol5 <oscar4293@gmail.com>
2026-05-25 03:21:31 +0900

    update

--- modules/embedding_modules.py
+++ modules/embedding_modules.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 from .data import TemporalGraphData,MemoryData
@@ -10,12 +11,7 @@ class EmbeddingModule(nn.Module):
             mem_dim:int=32,
             latent_dim:int=32,
             output_dim:int=32,
-            time_dim:int=32,
-            graph_data:TemporalGraphData=None,
             memory_data:MemoryData=None,
-            time_encoder:TimeEncoder=None,
-            n_head:int=1,
-            n_layer:int=1,
             is_memory:bool=True
         ):
         super().__init__()
@@ -23,16 +19,226 @@ class EmbeddingModule(nn.Module):
         self.mem_dim=mem_dim
         self.latent_dim=latent_dim
         self.output_dim=output_dim
-        self.time_dim=time_dim
-        self.graph_data=graph_data
         self.memory_data=memory_data
-        self.n_head=n_head
-        self.n_layer=n_layer
         self.is_memory=is_memory
-        self.time_encoder=time_encoder
 
     def compute_embedding(self):
         return NotImplemented
+
+class IdentityEmbedding(EmbeddingModule):
+    """
+    memory state를 node embedding으로 직접 사용
+    """
+    def __init__(self,
+            node_dim:int=32,
+            mem_dim:int=32,
+            latent_dim:int=32,
+            output_dim:int=32,
+            memory_data:MemoryData=None,
+        ):
+        super(IdentityEmbedding,self).__init__(
+            node_dim=node_dim,
+            mem_dim=mem_dim,
+            latent_dim=latent_dim,
+            output_dim=output_dim,
+            memory_data=memory_data,
+            is_memory=True
+        )
+    def compute_embedding(self,batch_tar):
+        batch_tar_ft=self.memory_data.get_batch_memory(batch_node=batch_tar) # [B,mem_dim]
+        return batch_tar_ft
+
+class TimeProjectionEmbedding(EmbeddingModule):
+    """
+    emb(i,t)=(1+delta_t*w) x s^t_i
+    """
+    def __init__(self,
+            node_dim:int=32,
+            mem_dim:int=32,
+            latent_dim:int=32,
+            output_dim:int=32,
+            memory_data:MemoryData=None,
+        ):
+        super(TimeProjectionEmbedding,self).__init__(
+            node_dim=node_dim,
+            mem_dim=mem_dim,
+            latent_dim=latent_dim,
+            output_dim=output_dim,
+            memory_data=memory_data,
+            is_memory=True
+        )
+        # time-projection embedding layer
+        class NormalLinear(nn.Linear):
+            # From Jodie code
+            def reset_parameters(self):
+                stdv=1./math.sqrt(self.weight.size(1))
+                self.weight.data.normal_(0,stdv)
+                if self.bias is not None:
+                    self.bias.data.normal_(0,stdv)
+        self.embedding_layer=NormalLinear(in_features=1,out_features=self.mem_dim)
+
+    def compute_embedding(self,batch_tar,batch_t):
+        batch_mem=self.memory_data.get_batch_memory(batch_node=batch_tar) # [B,mem_dim]
+        batch_ts=self.memory_data.get_batch_timespan(
+            batch_node=batch_tar,
+            batch_t=batch_t
+        ) # [B,1]
+        batch_tar_ft=batch_mem * (1+self.embedding_layer(batch_ts)) # [B,mem_dim]
+        return batch_tar_ft
+
+class GraphSumEmbedding(EmbeddingModule):
+    def __init__(self,
+            node_dim:int=32,
+            mem_dim:int=32,
+            latent_dim:int=32,
+            output_dim:int=32,
+            time_dim:int=32,
+            graph_data:TemporalGraphData=None,
+            memory_data:MemoryData=None,
+            time_encoder:TimeEncoder=None,
+            n_layer:int=1,
+            is_memory:bool=True
+        ):
+        super(GraphSumEmbedding,self).__init__(
+            node_dim=node_dim,
+            mem_dim=mem_dim,
+            latent_dim=latent_dim,
+            output_dim=output_dim,
+            memory_data=memory_data,
+            is_memory=is_memory
+        )
+        # parameter
+        self.time_dim=time_dim
+        self.n_layer=n_layer
+
+        # graph data
+        self.graph_data=graph_data
+
+        # module
+        self.time_encoder=time_encoder
+        base_dim=node_dim+mem_dim if self.is_memory else node_dim
+        self.linear_1=torch.nn.ModuleList([
+            nn.Linear(
+                in_features=(base_dim if idx==0 else output_dim)+time_dim,
+                out_features=output_dim
+            )
+            for idx in range(n_layer)
+        ])
+
+        self.linear_2=torch.nn.ModuleList([
+            nn.Linear(
+                in_features=(base_dim if idx==0 else output_dim)+time_dim+output_dim,
+                out_features=output_dim
+            )
+            for idx in range(n_layer)
+        ])
+        self.relu=nn.ReLU()
+
+    def compute_embedding(self,
+            batch_tar,
+            batch_t,
+            n_layer
+        ):
+        if n_layer==0:
+            if self.is_memory:
+                batch_tar_mem=self.memory_data.get_batch_memory(batch_node=batch_tar) # [B,mem_dim]
+                batch_tar_ft=self.graph_data.get_batch_node_feature(batch_node=batch_tar) # [B,node_dim]
+                batch_tar_ft=torch.concat(
+                    [batch_tar_mem,batch_tar_ft],
+                    dim=-1
+                ) # [B,mem_dim+node_dim]
+            else:
+                batch_tar_ft=self.graph_data.get_batch_node_feature(batch_node=batch_tar) # [B,node_dim]
+            return batch_tar_ft
+        else:
+            batch_tar_ft=self.compute_embedding(
+                batch_tar=batch_tar,
+                batch_t=batch_t,
+                n_layer=n_layer-1
+            )
+            embed_data=self.graph_data.get_data_for_embedding(
+                batch_tar=batch_tar,
+                batch_t=batch_t
+            )
+            batch_tar_ts=embed_data["batch_tar_ts"] # [B,]
+            batch_n=embed_data["batch_n"] # [B,N]
+            batch_n_t=embed_data["batch_n_t"] # [B,N] 
+            batch_n_ts=embed_data["batch_n_ts"] # [B,N]
+            batch_n_mask=embed_data["batch_n_mask"] # [B,N]
+
+            batch_size,max_n=batch_n.size()
+            batch_n=batch_n.flatten() # [B,N] -> [B x N,]
+            batch_n_t=batch_n_t.flatten() # [B,N] -> [B x N,]
+            n_embedding=self.compute_embedding(
+                batch_tar=batch_n,
+                batch_t=batch_n_t,
+                n_layer=n_layer-1
+            ) # [B x N,latent_dim]
+
+            ### aggregation
+            # time encoding
+            batch_tar_ts=batch_tar_ts.unsqueeze(-1) # -> [B,1]
+            batch_n_ts=batch_n_ts.unsqueeze(-1) # -> [B,N,1]
+            batch_tar_ts_ft=self.time_encoder(batch_tar_ts) # -> [B,time_dim]
+            batch_n_ts_ft=self.time_encoder(batch_n_ts) # -> [B,N,time_dim]
+
+            # reshape
+            n_embedding=n_embedding.reshape(batch_size,max_n,-1) # -> [B,N,latent_dim]
+
+            # aggregate
+            updated_batch_tar_ft=self.aggregate(
+                tar_ft=batch_tar_ft,
+                tar_ts_ft=batch_tar_ts_ft,
+                n_ft=n_embedding,
+                n_ts_ft=batch_n_ts_ft,
+                n_mask=batch_n_mask,
+                n_layer=n_layer
+            )
+            return updated_batch_tar_ft
+    
+    def aggregate(self,
+            tar_ft:torch.Tensor,
+            tar_ts_ft:torch.Tensor,
+            n_ft:torch.Tensor,
+            n_ts_ft:torch.Tensor,
+            n_mask:torch.Tensor,
+            n_layer:int):
+        """
+        """
+        # neighbor feature 구성
+        neighbor_ft=torch.concat(
+            [n_ft,n_ts_ft],
+            dim=-1
+        )
+
+        # 각 neighbor message 변환: W_1(...)
+        neighbor_msg=self.linear_1[n_layer-1](neighbor_ft) # [B,N,output_dim]
+
+        # padding neighbor 제거, n_mask가 valid=True라면 그대로 사용
+        neighbor_msg=neighbor_msg.masked_fill(
+            ~n_mask.unsqueeze(-1),
+            0.0
+        )
+
+        # sum aggregation
+        neighbor_sum=torch.sum(neighbor_msg,dim=1) # [B,output_dim]
+        neighbor_sum=self.relu(neighbor_sum)
+
+        # target node feature 구성
+        target_ft=torch.concat(
+            [tar_ft,tar_ts_ft],
+            dim=-1
+        )  # [B,tar_dim+time_dim]
+
+        # self feature와 neighbor aggregate 결합
+        output=torch.concat(
+            [target_ft,neighbor_sum],
+            dim=-1
+        )  # [B, tar_dim+time_dim+output_dim]
+
+        # W_2(...)
+        output=self.linear_2[n_layer-1](output) # [B,output_dim]
+        return output # [B,latent_dim]
 
 class GraphAttentionEmbedding(EmbeddingModule):
     def __init__(self,
@@ -53,25 +259,29 @@ class GraphAttentionEmbedding(EmbeddingModule):
             mem_dim=mem_dim,
             latent_dim=latent_dim,
             output_dim=output_dim,
-            time_dim=time_dim,
-            graph_data=graph_data,
             memory_data=memory_data,
-            time_encoder=time_encoder,
-            n_head=n_head,
-            n_layer=n_layer,
             is_memory=is_memory
         )
-        # attn module
+        # parameter
+        self.time_dim=time_dim
+        self.n_head=n_head
+        self.n_layer=n_layer
+
+        # graph data
+        self.graph_data=graph_data
+
+        # time,attn module
+        self.time_encoder=time_encoder
         layer_0_input_dim=node_dim+mem_dim if self.is_memory else node_dim
         self.attn_layers=torch.nn.ModuleList([
-            TemporalGraphAttention(
-                input_dim=layer_0_input_dim if idx==0 else output_dim,
-                latent_dim=latent_dim,
-                output_dim=output_dim,
-                time_dim=time_dim,
-                n_head=n_head
-            )
-        for idx in range(n_layer)])
+                TemporalGraphAttention(
+                    input_dim=layer_0_input_dim if idx==0 else output_dim,
+                    latent_dim=latent_dim,
+                    output_dim=output_dim,
+                    time_dim=time_dim,
+                    n_head=n_head
+                )
+            for idx in range(n_layer)])
 
     def compute_embedding(self,
             batch_tar,
--- test/test_4.py
+++ test/test_4.py
@@ -1,17 +1,18 @@
 import argparse
 import torch
 from modules import TemporalGraphData,MemoryData
-from modules import TimeEncoder,GraphAttentionEmbedding,GRUMemoryUpdater
+from modules import TimeEncoder,GRUMemoryUpdater
+from modules import IdentityEmbedding,TimeProjectionEmbedding,GraphAttentionEmbedding,GraphSumEmbedding
 
 """
 << Test >> 
-memory_modules.GraphAttentionEmbedding
+memory_modules.EmbeddingModule
 """
 def test_fn(**kwargs):
     match kwargs['test_num']:
         case 1:
             """
-            Test. GRUMemoryUpdater.update_memory
+            Test. GraphAttentionEmbedding
             """
             # parameter
             node_dim=4
@@ -96,11 +97,257 @@ def test_fn(**kwargs):
                     n_layer=3
                 )
 
+                print(f"Test Module: GraphAttentionEmbedding")
                 print(f"updated batch_tar_feature:")
                 print(updated_batch_tar_ft)
 
+        case 2:
+            """
+            Test. IdentityEmbedding
+            """
+            # parameter
+            node_dim=4
+            mem_dim=4
+            msg_dim=12 # msg_fn="concat"인 경우 mem_dim+mem_dim+time_dim
+            latent_dim=4
+            output_dim=4
+            time_dim=4
+            msg_fn="concat"
+            aggr_fn="last"
+            is_memory=True
+            batch_size=4
 
+            # data
+            eventstream=[
+                (3,7,1),
+                (3,6,2),
+                (1,4,3),
+                (1,3,4),
 
+                (1,2,5),
+                (2,7,5),
+                (1,4,7),
+                (4,7,7),
+
+                (4,5,8),
+                (1,3,9)
+            ]
+            memory_data=MemoryData(mem_dim=mem_dim)
+            time_encoder=TimeEncoder(time_dim=time_dim)
+            memory_updater=GRUMemoryUpdater(
+                mem_dim=mem_dim,
+                msg_dim=msg_dim,
+                time_dim=time_dim,
+                time_encoder=time_encoder,
+                memory_data=memory_data,
+                msg_fn=msg_fn,
+                aggr_fn=aggr_fn
+            )
+
+            embedding_module=IdentityEmbedding(
+                node_dim=node_dim,
+                mem_dim=mem_dim,
+                latent_dim=latent_dim,
+                output_dim=output_dim,
+                memory_data=memory_data
+            )
+            
+            for idx in range(0,len(eventstream),batch_size):
+                batch_events=eventstream[idx:idx+batch_size]
+
+                # update data
+                memory_data.update_memory_data(batch_events=batch_events)
+
+                ### execute memory, embed module
+                src,tar,event_t=zip(*batch_events)
+                src=torch.tensor(src,dtype=torch.long) # [B,]
+                tar=torch.tensor(tar,dtype=torch.long) # [B,]
+                event_t=torch.tensor(event_t,dtype=torch.float32) # [B,]
+
+                # 1. execute memory module
+                memory_updater.update_memory(
+                    src=src,
+                    tar=tar,
+                    event_t=event_t
+                )
+
+                # 2. execute embed module
+                updated_batch_tar_ft=embedding_module.compute_embedding(
+                    batch_tar=tar
+                )
+
+                print(f"Test Module: IdentityEmbedding")
+                print(f"updated batch_tar_feature:")
+                print(updated_batch_tar_ft)
+
+        case 3:
+            """
+            Test. TimeProjectionEmbedding
+            """
+            # parameter
+            node_dim=4
+            mem_dim=4
+            msg_dim=12 # msg_fn="concat"인 경우 mem_dim+mem_dim+time_dim
+            latent_dim=4
+            output_dim=4
+            time_dim=4
+            msg_fn="concat"
+            aggr_fn="last"
+            is_memory=True
+            batch_size=4
+
+            # data
+            eventstream=[
+                (3,7,1),
+                (3,6,2),
+                (1,4,3),
+                (1,3,4),
+
+                (1,2,5),
+                (2,7,5),
+                (1,4,7),
+                (4,7,7),
+
+                (4,5,8),
+                (1,3,9)
+            ]
+            memory_data=MemoryData(mem_dim=mem_dim)
+            time_encoder=TimeEncoder(time_dim=time_dim)
+            memory_updater=GRUMemoryUpdater(
+                mem_dim=mem_dim,
+                msg_dim=msg_dim,
+                time_dim=time_dim,
+                time_encoder=time_encoder,
+                memory_data=memory_data,
+                msg_fn=msg_fn,
+                aggr_fn=aggr_fn
+            )
+
+            embedding_module=TimeProjectionEmbedding(
+                node_dim=node_dim,
+                mem_dim=mem_dim,
+                latent_dim=latent_dim,
+                output_dim=output_dim,
+                memory_data=memory_data
+            )
+            
+            for idx in range(0,len(eventstream),batch_size):
+                batch_events=eventstream[idx:idx+batch_size]
+
+                # update data
+                memory_data.update_memory_data(batch_events=batch_events)
+
+                ### execute memory, embed module
+                src,tar,event_t=zip(*batch_events)
+                src=torch.tensor(src,dtype=torch.long) # [B,]
+                tar=torch.tensor(tar,dtype=torch.long) # [B,]
+                event_t=torch.tensor(event_t,dtype=torch.float32) # [B,]
+
+                # 1. execute memory module
+                memory_updater.update_memory(
+                    src=src,
+                    tar=tar,
+                    event_t=event_t
+                )
+
+                # 2. execute embed module
+                updated_batch_tar_ft=embedding_module.compute_embedding(
+                    batch_tar=tar,
+                    batch_t=event_t
+                )
+
+                print(f"Test Module: TimeProjectionEmbedding")
+                print(f"updated batch_tar_feature:")
+                print(updated_batch_tar_ft)
+
+        case 4:
+            """
+            Test. GraphSumEmbedding
+            """
+            # parameter
+            node_dim=4
+            mem_dim=4
+            msg_dim=12 # msg_fn="concat"인 경우 mem_dim+mem_dim+time_dim
+            latent_dim=4
+            output_dim=4
+            time_dim=4
+            n_layer=3
+            msg_fn="concat"
+            aggr_fn="last"
+            is_memory=True
+            batch_size=4
+
+            # data
+            eventstream=[
+                (3,7,1),
+                (3,6,2),
+                (1,4,3),
+                (1,3,4),
+
+                (1,2,5),
+                (2,7,5),
+                (1,4,7),
+                (4,7,7),
+
+                (4,5,8),
+                (1,3,9)
+            ]
+            graph_data=TemporalGraphData(node_dim=node_dim)
+            memory_data=MemoryData(mem_dim=mem_dim)
+            time_encoder=TimeEncoder(time_dim=time_dim)
+            memory_updater=GRUMemoryUpdater(
+                mem_dim=mem_dim,
+                msg_dim=msg_dim,
+                time_dim=time_dim,
+                time_encoder=time_encoder,
+                memory_data=memory_data,
+                msg_fn=msg_fn,
+                aggr_fn=aggr_fn
+            )
+
+            embedding_module=GraphSumEmbedding(
+                node_dim=node_dim,
+                mem_dim=mem_dim,
+                latent_dim=latent_dim,
+                output_dim=output_dim,
+                time_dim=time_dim,
+                graph_data=graph_data,
+                memory_data=memory_data,
+                time_encoder=time_encoder,
+                n_layer=n_layer,
+                is_memory=is_memory
+            )
+            
+            for idx in range(0,len(eventstream),batch_size):
+                batch_events=eventstream[idx:idx+batch_size]
+
+                # update data
+                graph_data.update_graph(batch_events=batch_events)
+                memory_data.update_memory_data(batch_events=batch_events)
+
+                ### execute memory, embed module
+                src,tar,event_t=zip(*batch_events)
+                src=torch.tensor(src,dtype=torch.long) # [B,]
+                tar=torch.tensor(tar,dtype=torch.long) # [B,]
+                event_t=torch.tensor(event_t,dtype=torch.float32) # [B,]
+
+                # 1. execute memory module
+                memory_updater.update_memory(
+                    src=src,
+                    tar=tar,
+                    event_t=event_t
+                )
+
+                # 2. execute embed module
+                updated_batch_tar_ft=embedding_module.compute_embedding(
+                    batch_tar=tar,
+                    batch_t=event_t,
+                    n_layer=3
+                )
+
+                print(f"Test Module: GraphSumEmbedding")
+                print(f"updated batch_tar_feature:")
+                print(updated_batch_tar_ft)
 
 if __name__=="__main__":
     """
